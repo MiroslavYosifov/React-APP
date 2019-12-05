cafab3f7fcca1c62435c69748b116bb48c2429e7
MiroslavYosifov <m.yosifov193@gmail.com>
2019-12-06 00:21:24 +0200

    add validation in edit cmp

--- Recipe-App/client-side/src/components/recipes/EditRecipe/EditRecipe.jsx
+++ Recipe-App/client-side/src/components/recipes/EditRecipe/EditRecipe.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './EditRecipe.css';
 import recipeService from '../../../services/recipe-service';
 import { Redirect } from 'react-router-dom';
+import * as yup from 'yup'; // for everything
+
+
 
 class EditRecipe extends React.Component {
     constructor (props) {
@@ -10,6 +13,7 @@ class EditRecipe extends React.Component {
                 title: '',
                 products: '',
                 imageUrl: '',
+                inputError: ''
             }
     }
 
@@ -35,16 +39,31 @@ class EditRecipe extends React.Component {
         e.preventDefault();
         const data = this.state;
         const recipeId = this.props.match.params.id;
+
+        schema.validate({ title: this.state.title, products: this.state.products, imageUrl: this.state.imageUrl})
+        .then(() => {
+            return this.state;
+        })
+        .catch((err) => {
+            console.log(err);
+            this.setState({inputError: err});
+        });
+        
         recipeService.editMyRecipe(data,recipeId).then(() => {
             let url = '/recipe/details/' + recipeId;
             return <Redirect to={url} />
         });
     }
 
+    // getFirstControlError = (name) => {
+    //     const errorState = this.state.inputError;
+    //     return errorState && errorState[name] && errorState[name][0];
+    //   };
+
     componentDidMount() {
         const recipeId = this.props.match.params.id;
         recipeService.getRecipe(recipeId).then(recipe => {
-            console.log('I AM 1',recipe);
+            console.log('I AM 1', recipe);
             this.setState({
                 title: recipe.title,
                 products: recipe.products,
@@ -53,9 +72,16 @@ class EditRecipe extends React.Component {
         });
     }
 
+
     render() {
-        const  { title, products, imageUrl } = this.state;
-                
+        const  { title, products, imageUrl, inputError} = this.state;
+        const titleError = inputError.path === 'title';
+        const productsError = inputError.path === 'products';
+        const imageUrlError = inputError.path === 'imageUrl';
+
+        // console.log('titleError', titleError);
+        // console.log('productsError', productsError);
+        // console.log('imageUrlError', imageUrlError);
         return (
             <section className="EditRecipeWrapper">
                 <header>
@@ -65,14 +91,17 @@ class EditRecipe extends React.Component {
                     <p>
                         <label htmlFor="title">Title</label>
                         <input type="text" onChange={this.changeTitle} value={title} id="title"/>
+                        {titleError && <span>{inputError.message}</span>}
                     </p>
                     <p>
                         <label htmlFor="products">Products</label>
                         <input type="text" onChange={this.changeProduct} value={products} id="products"/>
+                        {productsError && <span>{inputError.message}</span>}
                     </p>
                     <p>
                         <label htmlFor="imageUrl">Image URL</label>
                         <input type="text" onChange={this.changeImageUrl} value={imageUrl} id="imageUrl"/>
+                        {imageUrlError && <span>{inputError.message}</span>}
                     </p>
                     <p>
                         <button className="EditButton" type="submit">Edit Recipe</button>
@@ -83,5 +112,11 @@ class EditRecipe extends React.Component {
         )
     }
 };
+
+const schema = yup.object({
+    title: yup.string('Title should be string!').required('Title is required!').min(4, 'Title should be more than 4 characters!'),
+    products: yup.string('Products should be string!').required('Product is required!'),
+    imageUrl: yup.string('Image URL should be string!').required('Image URL is required!'),
+});
 
 export default EditRecipe;
